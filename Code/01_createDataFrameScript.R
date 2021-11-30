30e7ff3869c6ee92e214706b95eb72000093566c
people-r-strange <vmaxwell@smith.edu>
2021-11-30 15:31:35 -0500

    Renamed variables to make more sense

--- Code/01_createDataFrameScript.R
+++ Code/01_createDataFrameScript.R
@@ -2,35 +2,37 @@
 library(tidyverse)
 
 ## read in txt files automatically----
-fname <- list.files("OPUS", full.names = T) ###1:100
+fname <- list.files("Samples/greenlandSamples", full.names = T) 
+###Dimensions = 1:28 (Greenland) ###1:100 (Alaska)
 
 ###Components of Function 1-----
 ##List with all text files (Two methods depending on separator) 
 #filelist <- lapply(fname, read.delim, header = F) 
 #add , for alaska samples
-filelist <- lapply(fname, read.table, sep="") ### 100 [1:1882]
+filelist <- lapply(fname, read.table, sep="") 
+### Dimensions = 28 [1:3697] (Greenland) 100 [1:1882] (Alaska)
  
 ## Adding sample IDs (reference to lake core)
 names(filelist) <- gsub(".*/(.*)\\..*", "\\1", fname)
 
 ###Components of Function 2-------
-# save the transformed df to a new list of df called reformattedData [1:3697]
+# save the transformed df to a new list of df called reformattedData [1:3697] (Greenland)
 reformattedData <- lapply(filelist, function(x){pivot_wider(x, names_from = V1, values_from = V2)})
 
 # Unlist the reformattedData list into matrix (each of the 28 elements has one row of 3697 wavenumber values)
-allNames <- lapply(reformattedData, names)
+wavenumber2 <- lapply(reformattedData, names)
 
 # convert matrix into dataframe [28:3697]
-allNames2 <- as.data.frame(do.call("rbind",allNames))
-(allNames2$V1) #3996.31543 ALASKA
-(allNames2$V1882) #368.38622 ALASKA
+wavenumber <- as.data.frame(do.call("rbind",wavenumber2))
+(wavenumber$V1) #3996.31543 ALASKA
+(wavenumber$V1882) #368.38622 ALASKA
 
-(allNames2$V1) #7496.97825 // 7496.94889 GREENLAND
-(allNames2$V3697) #368.38766 GREENLAND
+(wavenumber$V1) #7496.97825 // 7496.94889 GREENLAND
+(wavenumber$V3697) #368.38766 GREENLAND
 
 
 # add row names permanently
-allNames2$dataset <- names(filelist) ## make this a specific column, don't trust it to store
+wavenumber$dataset <- names(filelist) ## make this a specific column, don't trust it to store
 
 #Rename column header from "wavenumbers" to "Vi" (FUNCTION #3)
 dropNames <- function(data){
@@ -39,11 +41,11 @@ dropNames <- function(data){
 }
 
 # creating new list of df where there aren't any wavenumbers...only absorbance values [1:3697]
-reformattedData2 <- lapply(reformattedData, dropNames)
+absorbanceValues2 <- lapply(reformattedData, dropNames)
 
 # Dataframe of [28:3697]where absorbance values are in cells
 ##need to resolve mismatch in wavenumbers before moving forward
-newData <- do.call(rbind.data.frame,reformattedData2)
+absorbanceValue <- do.call(rbind.data.frame,absorbanceValues2)
 
 lapply(reformattedData, ncol) %>% unlist() %>% summary()
 
@@ -54,21 +56,23 @@ lapply(reformattedData, ncol) %>% unlist() %>% summary()
 ###AW-34.5 (8_31_16).0  AW-7.5 (8_31_16).0   AW-73 (8_31_16).0 
 
 ## adds column for each row to remind us which file it is
-newData$dataset <- names(filelist)
+absorbanceValue$dataset <- names(filelist)
 
 ## Make data sample name in first column
-newData <- newData[,c(ncol(newData),1:(ncol(newData)-1))]
+wavenumber <- wavenumber[,c(ncol(wavenumber),1:(ncol(wavenumber)-1))] ### 28:3698
 
-allNames2 <- allNames2[,c(ncol(allNames2),1:(ncol(allNames2)-1))]
+absorbance <- absorbanceValue[,c(ncol(absorbanceValue),1:(ncol(absorbanceValue)-1))] ### 28:3698
 
-write.csv(newData, "newData.csv")
+write.csv(wavenumber, "csvFiles/wavenumber.csv")
+
+write.csv(absorbance, "csvFiles/absorbance.csv")
 
 ###Components of Function 4-----
 #Read in calibration csv with same number of samples as our transformedData 
-wet_chem_data <- read_csv("csvFiles/resolved-sample-name.csv") ###28
+wet_chem_data <- read_csv("csvFiles/wet-chem-data.csv") ###28
 
 #Read in absorbance values for each sample
-transformedData <- read_csv("csvFiles/transformedData.csv") ###28:3698
+absorbance <- read_csv("csvFiles/absorbance.csv") ###28:3698
 
 #Rename wet_chem_data columns 
 names(wet_chem_data)[1] <- "dataset"
